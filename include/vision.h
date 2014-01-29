29d56940f673bb2b23c031cb80ae1638f190d945
Stewart Waller <sjwaller@gmail.com>
2014-01-29 23:39:24 +0000

    vision dev

--- include/vision.h
+++ include/vision.h
@@ -4,7 +4,7 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/RegionOfInterest.h>
-#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
 
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
