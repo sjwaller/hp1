1abc1915e9c9fa9e980401fdd4b880be2ab022e0
Stewart Waller <sjwaller@gmail.com>
2014-01-30 00:51:07 +0000

    vision dev

--- src/vision.cpp
+++ src/vision.cpp
@@ -44,7 +44,7 @@ Vision::Vision()
 
 //  image_transport::TransportHints hints("compressed", ros::TransportHints());
 
-  image_sub = it.subscribe("/camera/image_raw", 1, &Vision::callback);
+  image_sub = it.subscribe("/camera/image_raw", 1, &Vision::callback, this);
 }
 
 void Vision::setup()
@@ -69,17 +69,17 @@ void Vision::publish()
 
 void Vision::callback(const sensor_msgs::ImageConstPtr& msg)
 {
-  cv_bridge::CvImagePtr cv_ptr;
+  // cv_bridge::CvImagePtr cv_ptr;
 
-  try
-  {
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("cv_bridge exception: %s", e.what());
-    return;
-  }
+  // try
+  // {
+  //   cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+  // }
+  // catch (cv_bridge::Exception& e)
+  // {
+  //   ROS_ERROR("cv_bridge exception: %s", e.what());
+  //   return;
+  // }
 
   // cv_bridge::CvImageConstPtr cv_ptr;
   // try
